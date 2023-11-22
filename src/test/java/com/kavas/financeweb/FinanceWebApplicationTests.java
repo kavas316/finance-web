c812d77a9abc35950d4e4285038c76d0b9519e47
ardakavas <kavasarda@hotmail.com>
2023-11-22 20:42:22 +0300

    Created Project with Maven.

---
+++ src/test/java/com/kavas/financeweb/FinanceWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kavas.financeweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
